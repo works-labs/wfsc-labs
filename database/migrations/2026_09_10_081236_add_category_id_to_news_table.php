362b7e1b2595e752a33153ad9ef5196c91673977
apikapikk <mmuwaffiqulihsan@gmail.com>
2026-09-13 11:00:37 +0700

    fix migrations

--- database/migrations/2026_09_10_081236_add_category_id_to_news_table.php
+++ database/migrations/2026_09_10_081236_add_category_id_to_news_table.php
@@ -7,19 +7,21 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up(): void
-{
-    Schema::table('news', function (Blueprint $table) {
-        $table->foreign('category_id')
-            ->references('id')
-            ->on('news_categories')
-            ->nullOnDelete();
-    });
-}
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->foreignId('category_id')
+                ->nullable()
+                ->after('author_id')
+                ->constrained('news_categories')
+                ->nullOnDelete();
+        });
+    }
 
     public function down(): void
-{
-    Schema::table('news', function (Blueprint $table) {
-        $table->dropForeign(['category_id']);
-    });
-}
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
+        });
+    }
 };
